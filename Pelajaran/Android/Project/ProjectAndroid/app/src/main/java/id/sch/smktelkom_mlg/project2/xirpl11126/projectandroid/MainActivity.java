a67de91e09c5f34229ef12f5c5e5dac5c94efe3b
Fahmi Akbar <fahmialiasakbar@gmail.com>
2017-03-28 14:57:31 +0700

    Menu Clear

--- Pelajaran/Android/Project/ProjectAndroid/app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl11126/projectandroid/MainActivity.java
+++ Pelajaran/Android/Project/ProjectAndroid/app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl11126/projectandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package id.sch.smktelkom_mlg.project2.xirpl11126.projectandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,37 +14,37 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.Thoharoh).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(MainActivity.this, ThoharohActivity.class));
             }
         });
         findViewById(R.id.Najis).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(MainActivity.this, NajisActivity.class));
             }
         });
         findViewById(R.id.Wudhu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                startActivity(new Intent(MainActivity.this, WudhuActivity.class));
             }
         });
         findViewById(R.id.Tayamum).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(MainActivity.this, TayamumActivity.class));
             }
         });
         findViewById(R.id.Siwak).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(MainActivity.this, SiwakActivity.class));
             }
         });
         findViewById(R.id.Mandi).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(MainActivity.this, MandiActivity.class));
             }
         });
 
