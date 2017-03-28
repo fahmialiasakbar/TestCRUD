e06ce2b86b07dbf8110fc8d149b2e4f1d4e662ab
Fahmi Akbar <fahmialiasakbar@gmail.com>
2017-03-28 14:23:46 +0700

    Initial commit

---
+++ Pelajaran/Android/Project/ProjectAndroid/app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl11126/projectandroid/MainActivity.java
@@ -0,0 +1,21 @@
+package id.sch.smktelkom_mlg.project2.xirpl11126.projectandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.Thoharoh).setOnClickListener(new View.OnClickListener());
+        findViewById(R.id.Najis).setOnClickListener(new View.OnClickListener());
+        findViewById(R.id.Wudhu).setOnClickListener(new View.OnClickListener());
+        findViewById(R.id.Tayamum).setOnClickListener(new View.OnClickListener());
+        findViewById(R.id.Siwak).setOnClickListener(new View.OnClickListener());
+        findViewById(R.id.Mandi).setOnClickListener(new View.OnClickListener());
+
+    }
+}
--- Pelajaran/Android/Project/Thoharoh/app/src/main/java/id/sch/smktelkom_mlg/project2/xiri11126/thoharoh/Wudhu.java
+++ Pelajaran/Android/Project/Thoharoh/app/src/main/java/id/sch/smktelkom_mlg/project2/xiri11126/thoharoh/Wudhu.java
@@ -4,6 +4,7 @@ package id.sch.smktelkom_mlg.project2.xiri11126.thoharoh;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -23,9 +24,17 @@ public class Wudhu extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_wudhu, container, false);
-        btSiwak = (Button) view.findViewById(R.id.sunnah);
-        return view;
+        Button Siwak = onCreateView().findViewById(R.id.Siwak).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
+
+
+
+
+
 
 }
